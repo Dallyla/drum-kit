1dcc6787e42e2d5cbcaf9a15002c844d97745dea
Dallyla Oliveira <dallyla.s.oliveira@gmail.com>
2020-08-01 19:11:17 -0300

    callback functions

--- index.js
+++ index.js
@@ -4,52 +4,75 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
         
         var buttonInnerHTML = this.innerHTML;
         
-        switch (buttonInnerHTML) {
-            case "w":
-                var tom1 = new Audio("sounds/tom-1.mp3");
-                tom1.play();
-                break;
-            
-            case "a":
-                var crash = new Audio("sounds/crash.mp3");
-                crash.play();
-                break;
+        makeSound(buttonInnerHTML);        
 
-            case "s":
-                var tom2 = new Audio("sounds/tom-2.mp3");
-                tom2.play();
-                break;
-            
-            case "d":
-                var kickBass = new Audio("sounds/kick-bass.mp3");
-                kickBass.play();
-                break;
-
-            case "j":
-                var tom3 = new Audio("sounds/tom-3.mp3");
-                tom3.play();
-                break;
-
-            case "k":
-                var snare = new Audio("sounds/snare.mp3");
-                snare.play();
-                break;
-            
-            case "l":
-                var tom4 = new Audio("sounds/tom-4.mp3");
-                tom4.play();
-                break;
-
-            default: console.log(buttonInnerHTML);
-                break;
-        }
-            
-            
+        buttonAnimation(buttonInnerHTML);
+        
     });
 
 }
 
+//nesse caso o this não funciona porque o evento não está atrelado a nenhum elemento específico?
+document.addEventListener("keydown", function(event) { // não é preciso adicionar o evento do teclado a nenhum elemento específico, pode-se adicionar ao documento pois todo ele ficará à espera de uma tecla pressionada.
+    
+    makeSound(event.key);
+
+    buttonAnimation(event.key);
+
+});
+
+function makeSound(key) {
+        
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        
+        case "a":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "s":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        
+        case "d":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+
+        case "j":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "k":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        default: console.log(buttonInnerHTML);
+            break;
+    }
+}
+
+function buttonAnimation(currentKey) {
+
+    var activeButton = document.querySelector("."+currentKey);
+    
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {             
+        activeButton.classList.remove("pressed");
+    }, 100);
 
 
-
-
+}
